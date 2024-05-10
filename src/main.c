b70187b1bb5a38a02393279adea448c0d99e5e40
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-05-10 16:14:32 +0200

    changed method to check string lenght

--- src/main.c
+++ src/main.c
@@ -15,22 +15,21 @@ int main()
     cJSON *root = cJSON_CreateObject();
     weather_init();
     display_init();
-    display_setValues(0,0,0,0);
-    float tempHumidLight[] = {0,0,0};
+    display_setValues(0, 0, 0, 0);
+    float tempHumidLight[] = {0, 0, 0};
     float tenSeconds = 10000;
     updateWeather(tempHumidLight);
-    cJSON_AddNumberToObject(root, "temperature", tempHumidLight[0]);
-    cJSON_AddNumberToObject(root, "humidity", tempHumidLight[1]);
-    cJSON_AddNumberToObject(root, "light", tempHumidLight[2]);
-
+    cJSON_AddNumberToObject(root, "temperature", (double)tempHumidLight[0]);
+    cJSON_AddNumberToObject(root, "humidity", (double)tempHumidLight[1]);
+    cJSON_AddNumberToObject(root, "light", (double)tempHumidLight[2]);
     char *jsonString = cJSON_Print(root);
-
-    size_t length = strlen(str);
-
-
+    size_t length = strlen(jsonString);
     wifi_command_TCP_transmit(jsonString, length);
+    display_int(9999);
+    
+
     // while (1)
-    // {        
+    // {
     // display_int((int16_t)(tempHumidLight[0]));
     // _delay_ms(tenSeconds);
     // display_int((int16_t)(tempHumidLight[1]));
@@ -39,8 +38,8 @@ int main()
     // _delay_ms(tenSeconds);
     // display_int(9999);
     // _delay_ms(tenSeconds);
-        // send tempHumidLight to backend
-        // weather.updateWeather(tempHumidLight) updates the three three minute averages
-        // this should probably not be in a while(true) loop, but that will be when we figure out connection
+    // send tempHumidLight to backend
+    // weather.updateWeather(tempHumidLight) updates the three three minute averages
+    // this should probably not be in a while(true) loop, but that will be when we figure out connection
     // }
 }
