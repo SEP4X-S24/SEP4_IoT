6000d278a85eab251db146a90d4a7290693ca520
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-05-10 15:19:01 +0200

    tpc transmission with Json

--- src/main.c
+++ src/main.c
@@ -5,31 +5,42 @@
 #include "wifi.h"
 #include "weather.h"
 #include "light.h"
+#include <cJSON.h>
 
 int main()
 {
-    // wifi_init();
-    // wifi_command_join_AP("cubeco", "arina123");
-    // wifi_command_create_TCP_connection("172.20.10.3", 24, NULL, NULL);
+    wifi_init();
+    wifi_command_join_AP("Como_33?", "Aquilina1");
+    wifi_command_create_TCP_connection("192.168.1.226", 80, NULL, NULL);
+    cJSON *root = cJSON_CreateObject();
     weather_init();
     display_init();
     display_setValues(0,0,0,0);
     float tempHumidLight[] = {0,0,0};
     float tenSeconds = 10000;
     updateWeather(tempHumidLight);
+    cJSON_AddNumberToObject(root, "temperature", tempHumidLight[0]);
+    cJSON_AddNumberToObject(root, "humidity", tempHumidLight[1]);
+    cJSON_AddNumberToObject(root, "light", tempHumidLight[2]);
 
-    while (1)
-    {        
-    display_int((int16_t)(tempHumidLight[0]));
-    _delay_ms(tenSeconds);
-    display_int((int16_t)(tempHumidLight[1]));
-    _delay_ms(tenSeconds);
-    display_int((int16_t)(tempHumidLight[2]));
-    _delay_ms(tenSeconds);
-    display_int(9999);
-    _delay_ms(tenSeconds);
-        //send tempHumidLight to backend
-        //weather.updateWeather(tempHumidLight) updates the three three minute averages
-        //this should probably not be in a while(true) loop, but that will be when we figure out connection
-    }
+    char *jsonString = cJSON_Print(root);
+
+    size_t length = strlen(str);
+
+
+    wifi_command_TCP_transmit(jsonString, length);
+    // while (1)
+    // {        
+    // display_int((int16_t)(tempHumidLight[0]));
+    // _delay_ms(tenSeconds);
+    // display_int((int16_t)(tempHumidLight[1]));
+    // _delay_ms(tenSeconds);
+    // display_int((int16_t)(tempHumidLight[2]));
+    // _delay_ms(tenSeconds);
+    // display_int(9999);
+    // _delay_ms(tenSeconds);
+        // send tempHumidLight to backend
+        // weather.updateWeather(tempHumidLight) updates the three three minute averages
+        // this should probably not be in a while(true) loop, but that will be when we figure out connection
+    // }
 }
