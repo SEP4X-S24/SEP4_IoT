b1bef30b1ba719d3cf6597fc313c4f30010671ce
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-05-14 13:33:56 +0200

    included envs and iot has no import errors

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <util/delay.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "display.h"
 #include "dht11.h"
@@ -7,12 +8,27 @@
 #include "light.h"
 #include <cJSON.h>
 #include <string.h>
+#include <az_iot_hub_client.h>
+#include <az_result.h>
+#include <az_span.h>
+
+static const char *connectionString = "HostName=weatherstation.azure-devices.net;DeviceId=weather-arduino;SharedAccessKey=wWZRLEYdHXcS/JMjykNYuQjEfd55nwUciAIoTGZ5ty8=";
 
 int main()
 {
     wifi_init();
     wifi_command_join_AP("Pixel de Hugo", "Lumia535");
-    wifi_command_create_TCP_connection("192.168.87.126",88, NULL, NULL);
+    //wifi_command_create_TCP_connection("192.168.87.126", 80, NULL, NULL);
+    az_iot_hub_client client;
+    az_iot_hub_client_options options = az_iot_hub_client_options_default();
+
+    az_span device_id = az_span_create_from_str("weather-arduino");
+    az_result result = az_iot_hub_client_init(&client, az_span_create_from_str(connectionString), device_id, &options);
+    if (az_result_failed(result))
+    {
+        printf("Failed to initialize client, az_result return code %04x\n", result);
+    }
+
     cJSON *root = cJSON_CreateObject();
     weather_init();
     display_init();
@@ -23,8 +39,15 @@ int main()
     cJSON_AddNumberToObject(root, "light", collectedValues.light);
     char *jsonString = cJSON_Print(root);
     size_t length = strlen(jsonString);
-    wifi_command_TCP_transmit((unsigned char*)jsonString, length);
+    wifi_command_TCP_transmit((unsigned char *)jsonString, length);
     display_int(9999);
+    result = az_log_set_message_callback(&client, az_span_create_from_str(jsonString), NULL);
+    if (az_result_failed(result))
+    {
+        printf("Failed to send message, az_result return code %04x\n", result);
+    }
+
+    delay(10000);
 
     while (1)
     {
@@ -37,5 +60,4 @@ int main()
         display_int(9999);
         _delay_ms(1000);
     }
-    
 }
