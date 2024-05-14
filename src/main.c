fb211b813b53fb9717be92c19c1ab53f4f67d722
Redver <116087243+Redver@users.noreply.github.com>
2024-05-14 11:39:59 +0200

    changes

--- src/main.c
+++ src/main.c
@@ -10,32 +10,14 @@
 
 int main()
 {
-    wifi_init();
-    wifi_command_join_AP("Norlys83766", "bas81ymer29");
-    wifi_command_create_TCP_connection("192.168.87.126",88, NULL, NULL);
-    cJSON *root = cJSON_CreateObject();
     weather_init();
     display_init();
-    display_setValues(0, 0, 0, 0);
-    TempHumidLight collectedValues = updateWeather();
-    cJSON_AddNumberToObject(root, "temperature", collectedValues.temp);
-    cJSON_AddNumberToObject(root, "humidity", collectedValues.humid);
-    cJSON_AddNumberToObject(root, "light", collectedValues.light);
-    char *jsonString = cJSON_Print(root);
-    size_t length = strlen(jsonString);
-    wifi_command_TCP_transmit((unsigned char*)jsonString, length);
-    display_int(9999);
+
 
     while (1)
     {
-        display_int(collectedValues.temp);
-        _delay_ms(1000);
-        display_int(collectedValues.humid);
-        _delay_ms(1000);
-        display_int(collectedValues.light);
-        _delay_ms(1000);
-        display_int(9999);
-        _delay_ms(1000);
+        display_int(light_read());
+        _delay_ms(2000);
     }
     
 }
