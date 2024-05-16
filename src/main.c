eff1aab302d693961d45c6366357c209ec64c896
00FA12 <114292738+00FA12@users.noreply.github.com>
2024-05-16 11:16:05 +0200

    delete stuff from azure

--- src/main.c
+++ src/main.c
@@ -8,26 +8,15 @@
 #include "light.h"
 #include <cJSON.h>
 #include <string.h>
-#include <az_iot_hub_client.h>
-#include <az_result.h>
-#include <az_span.h>
 
-static const char *connectionString = "HostName=weatherstation.azure-devices.net;DeviceId=weather-arduino;SharedAccessKey=wWZRLEYdHXcS/JMjykNYuQjEfd55nwUciAIoTGZ5ty8=";
+
 
 int main()
 {
     wifi_init();
-    wifi_command_join_AP("Pixel de Hugo", "Lumia535");
-    //wifi_command_create_TCP_connection("192.168.87.126", 80, NULL, NULL);
-    az_iot_hub_client client;
-    az_iot_hub_client_options options = az_iot_hub_client_options_default();
-
-    az_span device_id = az_span_create_from_str("weather-arduino");
-    az_result result = az_iot_hub_client_init(&client, az_span_create_from_str(connectionString), device_id, &options);
-    if (az_result_failed(result))
-    {
-        printf("Failed to initialize client, az_result return code %04x\n", result);
-    }
+    wifi_command_join_AP("Como_33?", "Aquilina1");
+    wifi_command_create_TCP_connection("https://weatherstation4dev.azurewebsites.net", 443, NULL, NULL);
+    wifi_command_TCP_transmit("")
 
     cJSON *root = cJSON_CreateObject();
     weather_init();
@@ -41,13 +30,8 @@ int main()
     size_t length = strlen(jsonString);
     wifi_command_TCP_transmit((unsigned char *)jsonString, length);
     display_int(9999);
-    result = az_log_set_message_callback(&client, az_span_create_from_str(jsonString), NULL);
-    if (az_result_failed(result))
-    {
-        printf("Failed to send message, az_result return code %04x\n", result);
-    }
 
-    delay(10000);
+    _delay_ms(10000);
 
     while (1)
     {
