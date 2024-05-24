f7050bcfa63f9a41489d4c0a94e247face2633a5
Redver <116087243+Redver@users.noreply.github.com>
2024-05-24 11:08:41 +0200

    addded ther padding and buffering to the encryption

--- src/main.c
+++ src/main.c
@@ -14,17 +14,19 @@
 #include <aes.h>
 
 // Define the connection parameters
-#define SSID "Norlys83766"
-#define psswd "bas81ymer29"
-#define address "20.13.143.114"
-#define port 2228
+#define SSID "Pixel de Hugo"
+#define psswd "Lumia535"
+#define address "192.168.114.113"
+#define port 88
+
+#define BLOCK_SIZE 16
 
 bool ping_timeout = false;
 int timeout_count = 0;
 struct AES_ctx my_AES_ctx;
 bool UnlockingApproved = false;
 char received_message[128];
-uint8_t key[] = "7a68etADEHERT23SGT56";
+uint8_t key[] = "5468617473206D79204B756E67204675";
 
 void init_all()
 {
@@ -32,6 +34,30 @@ void init_all()
     weather_init();
     display_init();
     AES_init_ctx(&my_AES_ctx, key);
+}
+
+// Function to pad the input to a multiple of the block size
+void pad_input(uint8_t *input, size_t *length)
+{
+    size_t padded_length = (*length + BLOCK_SIZE - 1) & ~(BLOCK_SIZE - 1);
+    memset(input + *length, 0, padded_length - *length);
+    *length = padded_length;
+}
+
+void AES_ECB_encrypt_buffer(struct AES_ctx *ctx, uint8_t *buf, size_t length)
+{
+    for (size_t i = 0; i < length; i += BLOCK_SIZE)
+    {
+        AES_ECB_encrypt(ctx, buf + i);
+    }
+}
+
+void AES_ECB_decrypt_buffer(struct AES_ctx *ctx, uint8_t *buf, size_t length)
+{
+    for (size_t i = 0; i < length; i += BLOCK_SIZE)
+    {
+        AES_ECB_decrypt(ctx, buf + i);
+    }
 }
 
 void update_data()
@@ -42,16 +68,26 @@ void update_data()
 
         TempHumidLight collectedValues = updateWeather();
 
-        // adding the values to the json
+
         cJSON_AddNumberToObject(json, "temperature", collectedValues.temp);
         cJSON_AddNumberToObject(json, "humidity", collectedValues.humid);
         cJSON_AddNumberToObject(json, "light", collectedValues.light);
         char *jsonString = cJSON_Print(json);
         size_t length = strlen(jsonString);
-        AES_ECB_encrypt(&my_AES_ctx, (uint8_t *)jsonString);
+
+
+        pad_input((uint8_t *)jsonString, &length);
+
+
+        AES_ECB_encrypt_buffer(&my_AES_ctx, (uint8_t *)jsonString, length);
+
         wifi_command_TCP_transmit((unsigned char *)jsonString, length);
-        AES_ECB_decrypt(&my_AES_ctx,(uint8_t*)jsonString);
-        cJSON_free(json);
+
+        AES_ECB_decrypt_buffer(&my_AES_ctx, (uint8_t *)jsonString, length);
+
+        cJSON_Delete(json);
+        free(jsonString);
+
         ping_timeout = false;
         timeout_count = 0;
     }
