d7ba4d8838f2e17911e0323677f960ad57221320
Redver <116087243+Redver@users.noreply.github.com>
2024-05-27 19:44:08 +0200

    fixing more stuff

--- src/main.c
+++ src/main.c
@@ -16,8 +16,8 @@
 // Define the connection parameters
 #define SSID "Norlys83766"
 #define psswd "bas81ymer29"
-#define address "192.168.87.126"
-#define port 88
+#define address "20.13.143.114"
+#define port 2228
 
 #define BLOCK_SIZE 16
 
@@ -26,7 +26,7 @@ int timeout_count = 0;
 struct AES_ctx my_AES_ctx;
 bool UnlockingApproved = false;
 char received_message[128];
-uint8_t key[] = "E3C39C72AC8D2";
+uint8_t key[] = "E3C39C72AC8D2AE";
 
 void init_all()
 {
@@ -36,7 +36,7 @@ void init_all()
     AES_init_ctx(&my_AES_ctx, key);
 }
 
-// Function to pad the input to a multiple of the block size
+// Function to pad the input to a multiple of the block size using PKCS#7 padding
 void pad_input(uint8_t *input, size_t *length)
 {
     size_t padding_needed = BLOCK_SIZE - (*length % BLOCK_SIZE);
@@ -49,6 +49,7 @@ void pad_input(uint8_t *input, size_t *length)
     *length = padded_length;
 }
 
+
 void AES_ECB_encrypt_buffer(struct AES_ctx *ctx, uint8_t *buf, size_t length)
 {
     for (size_t i = 0; i < length; i += BLOCK_SIZE)
@@ -57,13 +58,6 @@ void AES_ECB_encrypt_buffer(struct AES_ctx *ctx, uint8_t *buf, size_t length)
     }
 }
 
-void AES_ECB_decrypt_buffer(struct AES_ctx *ctx, uint8_t *buf, size_t length)
-{
-    for (size_t i = 0; i < length; i += BLOCK_SIZE)
-    {
-        AES_ECB_decrypt(ctx, buf + i);
-    }
-}
 
 void bin2hex(const uint8_t *bin, size_t len, char *hex) {
     for (size_t i = 0; i < len; i++) {
@@ -71,7 +65,6 @@ void bin2hex(const uint8_t *bin, size_t len, char *hex) {
     }
     hex[len * 2] = '\0'; // Null-terminate the hex string
 }
-
 
 void create_and_send_weather()
 {
@@ -81,7 +74,6 @@ void create_and_send_weather()
 
         TempHumidLight collectedValues = updateWeather();
 
-
         cJSON_AddNumberToObject(json, "temperature", collectedValues.temp);
         cJSON_AddNumberToObject(json, "humidity", collectedValues.humid);
         cJSON_AddNumberToObject(json, "light", collectedValues.light);
@@ -90,14 +82,13 @@ void create_and_send_weather()
     
         pad_input((uint8_t *)jsonString, &length);
 
-        char hex[length];
+        char hex[length * 2 + 1]; // Adjust the size of hex array to accommodate null-terminated string
+
         AES_ECB_encrypt_buffer(&my_AES_ctx, (uint8_t *)jsonString, length);
         
-        bin2hex(jsonString,length,hex);
+        bin2hex((uint8_t *)jsonString, length, hex);
 
-        wifi_command_TCP_transmit((unsigned char *)hex, length);
-
-        AES_ECB_decrypt_buffer(&my_AES_ctx, (uint8_t *)jsonString, length);
+        wifi_command_TCP_transmit((unsigned char *)hex, length * 2);
 
         cJSON_Delete(json);
         free(jsonString);
@@ -110,7 +101,7 @@ void create_and_send_weather()
 WIFI_ERROR_MESSAGE_t wifi_connect()
 {
     wifi_command_join_AP(SSID, psswd);
-    return wifi_command_create_TCP_connection(address, port, update_data, received_message);
+    return wifi_command_create_TCP_connection(address, port, create_and_send_weather, received_message);
 }
 
 int main()
@@ -128,10 +119,10 @@ int main()
             _delay_ms(500);
             display_int(7777);
             connection = wifi_connect();
-            _delay_ms(2000);
             display_int(1111);
         }
         display_int(8888);
+        create_and_send_weather();
         ping_timeout = false;
         while (!ping_timeout)
         {
